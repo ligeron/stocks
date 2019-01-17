15f72cf8bf8a6a5778892e512dafccf3366679e6
Roman Yarmolenko <devxpro@gmail.com>
2019-01-17 20:25:56 +0200

    Init Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+pip install virtualenv
+virtualenv stocks
+source stocks/bin/activate
+pip install -r requirements.txt
---
+++ app/sync.py
@@ -0,0 +1,58 @@
+import urllib, json
+import pandas as pd
+import pandas_datareader as web
+from datetime import datetime
+import unicodedata
+import numpy as np
+import os
+import pickle
+import requests
+
+STOCKS_URL = 'https://api.iextrading.com/1.0/ref-data/symbols'
+CHANNEL = 'iex'
+
+STOCKS_FOLDER = 'files/stocks'
+STOCK_NAMES_FILE = 'stock_names.json'
+
+DATE_FROM = datetime(2017, 1, 1)
+DATE_TO = datetime(2019, 12, 22)
+
+
+def to_string(u_string):
+    return unicodedata.normalize('NFKD', u_string).encode('ascii', 'ignore')
+
+
+def save_stocks(skip_exists):
+    folder = os.path.dirname(os.path.abspath(__file__)) + '/' + STOCKS_FOLDER + '/'
+    stock_names_file_path = folder + STOCK_NAMES_FILE
+
+    # Get stock names
+    if os.path.isfile(stock_names_file_path):
+        with open(stock_names_file_path) as stock_names_file:
+            stock_names = json.load(stock_names_file)
+    else:
+        response = requests.get(STOCKS_URL)
+        stock_names_json = response.content
+        stock_names_file = open(folder + STOCK_NAMES_FILE, "w")
+        stock_names_file.write(stock_names_json)
+        stock_names_file.close()
+        stock_names = json.loads(stock_names_json)
+
+    data = stock_names
+    count_stoks = len(data)
+    print count_stoks
+
+    for stock in data:
+        symbol = to_string(stock['symbol'])
+        name = to_string(stock['name'])
+        file_path = folder + symbol + '.csv'
+
+        if skip_exists and os.path.isfile(file_path):
+            print 'file for ' + name + ' already exists'
+            continue
+
+        stock_data = web.DataReader(symbol, CHANNEL, DATE_FROM, DATE_TO)['close']
+        stock_data.to_csv(file_path)
+        print name + ' was saved'
+
+save_stocks(True)
