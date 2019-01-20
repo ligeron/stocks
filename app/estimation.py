ee57eefa81b0674b86b80bd4573176ecdbf0b6c9
Roman Yarmolenko <devxpro@gmail.com>
2019-01-20 23:15:58 +0200

    add minimizer

--- app/estimation.py
+++ app/estimation.py
@@ -1,71 +1,114 @@
 from data_provider import get_stock_symbols, get_stock_data_by_symbols
 import numpy as np
+from settings import *
+from data_provider import to_string
+from scipy.optimize import minimize
 
-NUM_PORTS = 1
-
-
-def main():
-    symbols = get_stock_symbols(0)[100:101]
-    stocks = get_stock_data_by_symbols(symbols)
-    print stocks
-
-    print sharped_ratio_eval_random(stocks)
+NUM_PORTS = 1000
 
 
-def get_sharped_ratio_by_stock(symbol):
-    stocks = get_stock_data_by_symbols([symbol])
-    log_ret = np.log(stocks / stocks.shift(1))
-    ret_arr = np.sum((log_ret.mean() * 1) * 252)
-    vol_arr = np.sqrt(np.dot(1, np.dot(log_ret.cov() * 252, 1)))
-    sharpe_arr = ret_arr / vol_arr
+class Estimation:
+    stocks = None
 
-    return sharpe_arr.max()
+    def __init__(self):
+        symbols = self.get_best_stocks(30).values()
+        self.stocks = get_stock_data_by_symbols(symbols)
+
+    def main(self):
+        print self.sharped_ratio_eval_random(self.stocks).max()
+
+    def get_ret_vol_sr(self, weights):
+        log_ret = np.log(self.stocks / self.stocks.shift(1))
+        weights = np.array(weights)
+        ret = np.sum(log_ret.mean() * weights) * 252
+        vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+        sr = ret / vol
+        return np.array([ret, vol, sr])
+
+    def neg_sharpe(self, weights):
+        return self.get_ret_vol_sr(weights)[2] * -1
+
+    def check_sum(self, weights):
+        return np.sum(weights) - 1
+
+    def minimize_volatility(self, weights):
+        return self.get_ret_vol_sr(weights)[1]
+
+    def sharped_ratio_eval_minimizer(self):
+        stocks_count = len(self.stocks.columns)
+        frontier_y = np.linspace(0, 0.3, 3)
+        frontier_volatility = []
+
+        for possible_return in frontier_y:
+            # function for return
+            cons = ({'type': 'eq', 'fun': self.check_sum},
+                    {'type': 'eq', 'fun': lambda w: self.get_ret_vol_sr(w)[0] - possible_return})
+            bound_template = (0, 1)
+            i = 0
+            guess = 1 / float(stocks_count)
+            bounds = []
+            init_guess = []
+            while i < stocks_count:
+                bounds.append(bound_template)
+                init_guess.append(guess)
+                i += 1
+
+            bounds = tuple(bounds)
+
+            result = minimize(self.minimize_volatility, init_guess, method='SLSQP', bounds=bounds, constraints=cons)
+
+            frontier_volatility.append(result['fun'])
+        print frontier_volatility
+
+    def get_sharped_ratio_by_stock(self, symbol):
+        stocks = get_stock_data_by_symbols([symbol])
+        log_ret = np.log(stocks / stocks.shift(1))
+        ret_arr = np.sum((log_ret.mean() * 1) * 252)
+        vol_arr = np.sqrt(np.dot(1, np.dot(log_ret.cov() * 252, 1)))
+        sharpe_arr = ret_arr / vol_arr
+
+        return sharpe_arr.max()
+
+    def sharped_ratio_eval_random(self, stocks):
+        log_ret = np.log(stocks / stocks.shift(1))
+        all_weights = np.zeros((NUM_PORTS, len(stocks.columns)))
+        ret_arr = np.zeros(NUM_PORTS)
+        vol_arr = np.zeros(NUM_PORTS)
+        sharpe_arr = np.zeros(NUM_PORTS)
+
+        for ind in range(NUM_PORTS):
+            weights = np.array(np.random.random(len(stocks.columns)))
+            weights = weights / np.sum(weights)
+            all_weights[ind, :] = weights
+            ret_arr[ind] = np.sum((log_ret.mean() * weights) * 252)
+            vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
+            sharpe_arr[ind] = ret_arr[ind] / vol_arr[ind]
+
+        return sharpe_arr
+
+    def get_best_stocks(self, limit):
+        if os.path.isfile(SR_FILE_PATH):
+            with open(SR_FILE_PATH) as file:
+                sorted_stocks = json.load(file)
+        else:
+            sorted_stocks = {}
+            symbols = get_stock_symbols(0)[1:2500]
+            for symbol in symbols:
+                sorted_stocks[self.get_sharped_ratio_by_stock(symbol)] = symbol
+            file = open(SR_FILE_PATH, "w")
+            file.write(json.dumps(sorted_stocks))
+            file.close()
+
+        i = 1
+        result = {}
+        for key in sorted(sorted_stocks, reverse=True):
+            if i > limit:
+                break
+            result[key] = to_string(sorted_stocks[key])
+            i += 1
+
+        return result
 
 
-def sharped_ratio_eval_random(stocks):
-    log_ret = np.log(stocks / stocks.shift(1))
-    all_weights = np.zeros((NUM_PORTS, len(stocks.columns)))
-    ret_arr = np.zeros(NUM_PORTS)
-    vol_arr = np.zeros(NUM_PORTS)
-    sharpe_arr = np.zeros(NUM_PORTS)
-
-    for ind in range(NUM_PORTS):
-        # Create Random Weights
-        weights = np.array(np.random.random(len(stocks.columns)))
-
-        # Rebalance Weights
-        weights = weights / np.sum(weights)
-
-        # Save Weights
-        all_weights[ind, :] = weights
-
-        # Expected Return
-        ret_arr[ind] = np.sum((log_ret.mean() * weights) * 252)
-
-        # Expected Variance
-        vol_arr[ind] = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov() * 252, weights)))
-
-        # Sharpe Ratio
-        sharpe_arr[ind] = ret_arr[ind] / vol_arr[ind]
-
-    return sharpe_arr.max()
-    # return all_weights[sharpe_arr.argmax(),:]
-
-
-def get_best_stocks(limit):
-    sorted_stocks = {}
-    symbols = get_stock_symbols(0)[1:500]
-    for symbol in symbols:
-        sorted_stocks[get_sharped_ratio_by_stock(symbol)] = symbol
-    i = 1
-    result = {}
-    for key in sorted(sorted_stocks, reverse=True):
-        if i > limit:
-            break
-        result[key] = sorted_stocks[key]
-        i += 1
-    return result
-
-# main()
-x = get_best_stocks(4)
-x = get_best_stocks(4)
+x = Estimation()
+x.sharped_ratio_eval_minimizer()
--- app/settings.py
+++ app/settings.py
@@ -1,14 +1,17 @@
 from datetime import datetime
 import os
+import json
 
 STOCKS_URL = 'https://api.iextrading.com/1.0/ref-data/symbols'
 CHANNEL = 'iex'
 
 STOCKS_FOLDER = 'files/stocks'
 STOCK_NAMES_FILE = 'stock_names.json'
+SR_FILE = '!sr_by_company.json'
 
 DATE_FROM = datetime(2017, 1, 1)
 DATE_TO = datetime(2019, 12, 22)
 
 FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/' + STOCKS_FOLDER + '/'
 STOCK_NAMES_FILE_PATH = FOLDER + STOCK_NAMES_FILE
+SR_FILE_PATH = FOLDER + SR_FILE
