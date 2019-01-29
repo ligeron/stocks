59ecf8cfdd015c5bb986bf3694194f226d4dec86
Roman Yarmolenko <devxpro@gmail.com>
2019-01-29 15:38:20 +0200

    number_fo_stocks

--- app/checker.py
+++ app/checker.py
@@ -9,15 +9,15 @@ class MyEncoder(JSONEncoder):
         return o.__dict__
 
 
-amount = 100000
+amount = 2000
 DATE_MASK = '%Y-%m-%d'
 
-buy_date = '2019-01-03'
+buy_date = '2018-01-03'
 sell_date = '2019-11-22'
 buy_date_obj = datetime.strptime(buy_date, DATE_MASK)
 sell_date_obj = datetime.strptime(sell_date, DATE_MASK)
 
-ps = PortfolioSet('2017-01-03', '2018-01-03', total_stocks_count=20, portfolios_count=2)
+ps = PortfolioSet('2017-01-03', '2018-01-03', total_stocks_count=15, portfolios_count=2)
 
 print 'Calculate weights...'
 ps.process_weights()
@@ -26,7 +26,7 @@ print 'Calculate sell dynamic...'
 current_sell_date_obj = datetime.strptime(buy_date, DATE_MASK)
 while current_sell_date_obj < sell_date_obj:
     for portfolio in ps.portfolios:
-        portfolio.investments_amount = 100000
+        portfolio.investments_amount = amount
         portfolio.buy_date = buy_date
         portfolio.calculate_number_of_stocks()
         portfolio.add_portfolio_rev(current_sell_date_obj.strftime(DATE_MASK))
